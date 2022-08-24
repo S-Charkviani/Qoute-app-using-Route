967f30fe38cf94930d70759eb0a0778aaf5515b1
Salome Charkviani <salo.charkviani@gmail.com>
2022-08-24 20:55:27 +0400

    29

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -19,11 +19,16 @@ const QuoteList = (props) => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const isSortingAscending = queryParams.get("sort") === "asc";
+  
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
 
   const changeSortingHandler = () => {
-    history.push("/quotes?sort=" + (isSortingAscending ? "desc" : "asc"));
+history.push({
+  pathname: location.pathname,
+  search: `?sort=${isSortingAscending ? "desc" : "asc"}`
+})
   };
+
   return (
     <Fragment>
       <div className={classes.sorting}>
