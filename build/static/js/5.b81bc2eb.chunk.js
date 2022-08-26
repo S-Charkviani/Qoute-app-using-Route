b04f5e57c5bf500943ed9974a46bdbb713375d3a
Salome Charkviani <salo.charkviani@gmail.com>
2022-08-26 15:02:41 +0400

    26

---
+++ build/static/js/5.b81bc2eb.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-complete-guide"]=this["webpackJsonpreact-complete-guide"]||[]).push([[5],{54:function(e,t,c){"use strict";c.r(t);c(1);var n=c(0);t.default=function(){return Object(n.jsx)("div",{children:"Page not found!"})}}}]);
+//# sourceMappingURL=5.b81bc2eb.chunk.js.map
--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
+import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-// import MainNavigation from "./components/layout/MainNavigation";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import NotFound from "./pages/NotFound";
-import QuoteDetail from "./pages/QuoteDetail";
+
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
 
 function App() {
   // const [enteredQuote, setEnteredQuote] = useState({});
@@ -24,22 +26,41 @@ function App() {
 
   return (
     <Layout>
-  <Switch>   {/*<Routes> in React-rout 6 instead of Switch and 'Routs' nests every Rout*/}
-      
-<Route path="/" exact>
-  <Redirect to='/quotes' />   {/* 'Navigate to' instead of 'Redirect to' in Route 6*/ }
-</Route>
-      <Route path="/quotes" exact>  {/*instead of exact we need /* after link in Route 6 instead*/  }
-        <AllQuotes/>                
-      </Route>
-      <Route path="/quotes/:quoteId">   {/*element={<QuoteDetail/>}  in React-route 6 instead*/  }
-        <QuoteDetail />
-      </Route>
-      <Route path="/new-quote">     {/* Route 6 checks the whole matching of the link*/}
-        <NewQuote />
-      </Route>
-      <Route path='*'><NotFound /></Route>
-    </Switch>
+      {/*Needed for lazy code */}
+      <Suspense            
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          {" "}
+          {/*<Routes> in React-rout 6 instead of Switch and 'Routs' nests every Rout*/}
+          <Route path="/" exact>
+            <Redirect to="/quotes" />{" "}
+            {/* 'Navigate to' instead of 'Redirect to' in Route 6*/}
+          </Route>
+          <Route path="/quotes" exact>
+            {" "}
+            {/*instead of exact we need /* after link in Route 6 instead*/}
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            {" "}
+            {/*element={<QuoteDetail/>}  in React-route 6 instead*/}
+            <QuoteDetail />
+          </Route>
+          <Route path="/new-quote">
+            {" "}
+            {/* Route 6 checks the whole matching of the link*/}
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
